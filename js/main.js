e0f8ed9e418b2011c29f826bc042bbd1128ce2d6
Ionut Birlad <birladionut@yahoo.it>
2020-02-17 16:02:16 +0100

    versione originale

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+// Versione ORIGINALE
+var nome;
+var cognome;
+var colore;
+nome = prompt("Inserisci il tuo nome");
+cognome = prompt("Inserisci il tuo cognome");
+colore = prompt("Inserisci il tuo colore preferito");
+// console.log(nome);
+// console.log(cognome);
+// console.log(colore);
+document.getElementById("pass").innerHTML = " " + nome + cognome + colore + "20";
+// Fine versione ORIGINALE
