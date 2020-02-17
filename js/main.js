8a989368f13f4a4a88a33f744939a301d3249ade
Ionut Birlad <birladionut@yahoo.it>
2020-02-17 16:11:30 +0100

    versione con la data corrente

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 // Versione ORIGINALE
-var nome;
-var cognome;
-var colore;
-nome = prompt("Inserisci il tuo nome");
-cognome = prompt("Inserisci il tuo cognome");
-colore = prompt("Inserisci il tuo colore preferito");
-// console.log(nome);
-// console.log(cognome);
-// console.log(colore);
-document.getElementById("pass").innerHTML = " " + nome + cognome + colore + "20";
+
+// var nome;
+// var cognome;
+// var colore;
+// nome = prompt("Inserisci il tuo nome");
+// cognome = prompt("Inserisci il tuo cognome");
+// colore = prompt("Inserisci il tuo colore preferito");
+// // console.log(nome);
+// // console.log(cognome);
+// // console.log(colore);
+// document.getElementById("pass").innerHTML = " " + nome + cognome + colore + "20";
+
 // Fine versione ORIGINALE
+
+// Versione con la DATA CORRENTE
+
+// var nome = prompt("Inserisci il tuo nome");
+// var cognome = prompt("Inserisci il tuo cognome");
+// var colore = prompt("Inserisci il tuo colore preferito");
+// var data = new Date()
+// // console.log(data);
+// var annoCorrente = data.getFullYear()
+// // console.log(annoCorrente);
+// document.getElementById('pass').innerHTML = " " + nome + cognome + colore + annoCorrente;
+
+// fine versione con la DATA CORRENTE
