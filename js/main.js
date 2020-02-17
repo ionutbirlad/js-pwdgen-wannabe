0e1685db40a9460b4a03f49c400414fee3f236b4
Ionut Birlad <birladionut@yahoo.it>
2020-02-17 16:13:55 +0100

    correzione

--- js/main.js
+++ js/main.js
@@ -15,13 +15,13 @@
 
 // Versione con la DATA CORRENTE
 
-// var nome = prompt("Inserisci il tuo nome");
-// var cognome = prompt("Inserisci il tuo cognome");
-// var colore = prompt("Inserisci il tuo colore preferito");
-// var data = new Date()
-// // console.log(data);
-// var annoCorrente = data.getFullYear()
-// // console.log(annoCorrente);
-// document.getElementById('pass').innerHTML = " " + nome + cognome + colore + annoCorrente;
+var nome = prompt("Inserisci il tuo nome");
+var cognome = prompt("Inserisci il tuo cognome");
+var colore = prompt("Inserisci il tuo colore preferito");
+var data = new Date()
+// console.log(data);
+var annoCorrente = data.getFullYear()
+// console.log(annoCorrente);
+document.getElementById('pass').innerHTML = " " + nome + ";" + cognome + ";" + colore + ";" + annoCorrente;
 
 // fine versione con la DATA CORRENTE
