db3fb9420051b2164ff618116e933c9b789e2a8d
Ionut Birlad <birladionut@yahoo.it>
2020-02-17 16:18:53 +0100

    versione con la costante

--- js/main.js
+++ js/main.js
@@ -13,15 +13,27 @@
 
 // Fine versione ORIGINALE
 
+
 // Versione con la DATA CORRENTE
 
-var nome = prompt("Inserisci il tuo nome");
-var cognome = prompt("Inserisci il tuo cognome");
-var colore = prompt("Inserisci il tuo colore preferito");
-var data = new Date()
-// console.log(data);
-var annoCorrente = data.getFullYear()
-// console.log(annoCorrente);
-document.getElementById('pass').innerHTML = " " + nome + ";" + cognome + ";" + colore + ";" + annoCorrente;
+// var nome = prompt("Inserisci il tuo nome");
+// var cognome = prompt("Inserisci il tuo cognome");
+// var colore = prompt("Inserisci il tuo colore preferito");
+// var data = new Date()
+// // console.log(data);
+// var annoCorrente = data.getFullYear()
+// // console.log(annoCorrente);
+// document.getElementById('pass').innerHTML = " " + nome + ";" + cognome + ";" + colore + ";" + annoCorrente;
 
 // fine versione con la DATA CORRENTE
+
+
+// Versione con la COSTANTE
+
+var nome = prompt("Inserisci il tuo nome")
+var cognome = prompt("Inserisci il tuo cognome")
+var colore = prompt("Inserisci il tuo colore preferito")
+const twenty = 20;
+document.getElementById('pass').innerHTML = " " + nome + ";" + cognome + ";" + colore + ";" + twenty;
+
+// fine versione con la COSTANTE
